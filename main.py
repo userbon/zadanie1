a0a4921d081a6aec9e6849fd22ac1d715f061557
Maxim <riabo@LAPTOP-7QOUA2KS.(none)>
2022-02-17 19:26:10 +0100

    zadanie1

---
+++ main.py
@@ -0,0 +1,252 @@
+
+kwota_pozyczki = int(12000)
+oprocentowanie = int(3)
+rata = int(200)
+
+miesiac = input()
+inflacja = float(input())
+
+tpl = " w {} twoja pozostala kwota kredytu  to {},to 0 {} mniej niz w poprzednim miesiacu"
+
+splata1 = (1 + (inflacja + oprocentowanie)/1200) * kwota_pozyczki - rata
+roznica = kwota_pozyczki - splata1
+
+splata_zaok = round(splata1)
+roznica_zaok = round(roznica)
+print(tpl.format(miesiac,splata_zaok,roznica_zaok))
+
+
+miesiac = input()
+inflacja = float(input())
+
+splata2 = (1 + (inflacja + oprocentowanie)/1200) * splata1 - rata
+roznica = splata1 - splata2
+
+splata_zaok = round(splata2)
+roznica_zaok = round(roznica)
+print(tpl.format(miesiac,splata_zaok,roznica_zaok))
+
+
+miesiac = input()
+inflacja = float(input())
+
+splata3 = (1 + (inflacja + oprocentowanie)/1200) * splata2 - rata
+roznica = splata2 - splata3
+
+splata_zaok = round(splata3)
+roznica_zaok = round(roznica)
+print(tpl.format(miesiac,splata_zaok,roznica_zaok))
+
+miesiac = input()
+inflacja = float(input())
+
+splata4 = (1 + (inflacja + oprocentowanie)/1200) * splata3 - rata
+roznica = splata3 - splata4
+
+splata_zaok = round(splata4)
+roznica_zaok = round(roznica)
+print(tpl.format(miesiac,splata_zaok,roznica_zaok))
+
+miesiac = input()
+inflacja = float(input())
+
+splata5 = (1 + (inflacja + oprocentowanie)/1200) * splata4 - rata
+roznica = splata4 - splata5
+
+splata_zaok = round(splata5)
+roznica_zaok = round(roznica)
+print(tpl.format(miesiac,splata_zaok,roznica_zaok))
+
+miesiac = input()
+inflacja = float(input())
+
+splata6 = (1 + (inflacja + oprocentowanie)/1200) * splata5 - rata
+roznica = splata5 - splata6
+
+splata_zaok = round(splata6)
+roznica_zaok = round(roznica)
+print(tpl.format(miesiac,splata_zaok,roznica_zaok))
+
+miesiac = input()
+inflacja = float(input())
+
+splata7 = (1 + (inflacja + oprocentowanie)/1200) * splata6 - rata
+roznica = splata6 - splata7
+
+splata_zaok = round(splata7)
+roznica_zaok = round(roznica)
+print(tpl.format(miesiac,splata_zaok,roznica_zaok))
+
+miesiac = input()
+inflacja = float(input())
+
+splata8 = (1 + (inflacja + oprocentowanie)/1200) * splata7 - rata
+roznica = splata7 - splata8
+
+splata_zaok = round(splata8)
+roznica_zaok = round(roznica)
+print(tpl.format(miesiac,splata_zaok,roznica_zaok))
+
+miesiac = input()
+inflacja = float(input())
+
+splata9 = (1 + (inflacja + oprocentowanie)/1200) * splata8 - rata
+roznica = splata8 - splata9
+
+splata_zaok = round(splata9)
+roznica_zaok = round(roznica)
+print(tpl.format(miesiac,splata_zaok,roznica_zaok))
+
+miesiac = input()
+inflacja = float(input())
+
+splata10 = (1 + (inflacja + oprocentowanie)/1200) * splata9 - rata
+roznica = splata9 - splata10
+
+splata_zaok = round(splata10)
+roznica_zaok = round(roznica)
+print(tpl.format(miesiac,splata_zaok,roznica_zaok))
+
+miesiac = input()
+inflacja = float(input())
+
+splata11 = (1 + (inflacja + oprocentowanie)/1200) * splata10 - rata
+roznica = splata10 - splata11
+
+splata_zaok = round(splata11)
+roznica_zaok = round(roznica)
+print(tpl.format(miesiac,splata_zaok,roznica_zaok))
+
+miesiac = input()
+inflacja = float(input())
+
+splata12 = (1 + (inflacja + oprocentowanie)/1200) * splata11 - rata
+roznica = splata11 - splata12
+
+splata_zaok = round(splata12)
+roznica_zaok = round(roznica)
+print(tpl.format(miesiac,splata_zaok,roznica_zaok))
+
+miesiac = input()
+inflacja = float(input())
+
+splata13 = (1 + (inflacja + oprocentowanie)/1200) * splata12 - rata
+roznica = splata12 - splata13
+
+splata_zaok = round(splata13)
+roznica_zaok = round(roznica)
+print(tpl.format(miesiac,splata_zaok,roznica_zaok))
+
+miesiac = input()
+inflacja = float(input())
+
+splata14 = (1 + (inflacja + oprocentowanie)/1200) * splata13 - rata
+roznica = splata13 - splata14
+
+splata_zaok = round(splata14)
+roznica_zaok = round(roznica)
+print(tpl.format(miesiac,splata_zaok,roznica_zaok))
+
+miesiac = input()
+inflacja = float(input())
+
+splata15 = (1 + (inflacja + oprocentowanie)/1200) * splata14 - rata
+roznica = splata14 - splata15
+
+splata_zaok = round(splata15)
+roznica_zaok = round(roznica)
+print(tpl.format(miesiac,splata_zaok,roznica_zaok))
+
+miesiac = input()
+inflacja = float(input())
+
+splata16 = (1 + (inflacja + oprocentowanie)/1200) * splata15 - rata
+roznica = splata15 - splata16
+
+splata_zaok = round(splata16)
+roznica_zaok = round(roznica)
+print(tpl.format(miesiac,splata_zaok,roznica_zaok))
+
+miesiac = input()
+inflacja = float(input())
+
+splata17 = (1 + (inflacja + oprocentowanie)/1200) * splata16 - rata
+roznica = splata16 - splata17
+
+splata_zaok = round(splata17)
+roznica_zaok = round(roznica)
+print(tpl.format(miesiac,splata_zaok,roznica_zaok))
+
+miesiac = input()
+inflacja = float(input())
+
+splata18 = (1 + (inflacja + oprocentowanie)/1200) * splata17 - rata
+roznica = splata17 - splata18
+
+splata_zaok = round(splata18)
+roznica_zaok = round(roznica)
+print(tpl.format(miesiac,splata_zaok,roznica_zaok))
+
+miesiac = input()
+inflacja = float(input())
+
+splata19 = (1 + (inflacja + oprocentowanie)/1200) * splata18 - rata
+roznica = splata18 - splata19
+
+splata_zaok = round(splata19)
+roznica_zaok = round(roznica)
+print(tpl.format(miesiac,splata_zaok,roznica_zaok))
+
+miesiac = input()
+inflacja = float(input())
+
+splata20 = (1 + (inflacja + oprocentowanie)/1200) * splata19 - rata
+roznica = splata19 - splata20
+
+splata_zaok = round(splata20)
+roznica_zaok = round(roznica)
+print(tpl.format(miesiac,splata_zaok,roznica_zaok))
+
+miesiac = input()
+inflacja = float(input())
+
+splata21 = (1 + (inflacja + oprocentowanie)/1200) * splata20 - rata
+roznica = splata20 - splata21
+
+splata_zaok = round(splata21)
+roznica_zaok = round(roznica)
+print(tpl.format(miesiac,splata_zaok,roznica_zaok))
+
+miesiac = input()
+inflacja = float(input())
+
+splata22 = (1 + (inflacja + oprocentowanie)/1200) * splata21 - rata
+roznica = splata21 - splata22
+
+splata_zaok = round(splata22)
+roznica_zaok = round(roznica)
+print(tpl.format(miesiac,splata_zaok,roznica_zaok))
+
+miesiac = input()
+inflacja = float(input())
+
+splata23 = (1 + (inflacja + oprocentowanie)/1200) * splata22 - rata
+roznica = splata22 - splata23
+
+splata_zaok = round(splata23)
+roznica_zaok = round(roznica)
+print(tpl.format(miesiac,splata_zaok,roznica_zaok))
+
+miesiac = input()
+inflacja = float(input())
+
+splata24 = (1 + (inflacja + oprocentowanie)/1200) * splata23 - rata
+roznica = splata23 - splata24
+
+splata_zaok = round(splata24)
+roznica_zaok = round(roznica)
+print(tpl.format(miesiac,splata_zaok,roznica_zaok))
+
+
+
+
